19babccbc147dc11216106244160085dff9a7203
chlgpdus921 <chlgpdus921@naver.com>
2018-05-08 22:32:33 +0900

    practice

---
+++ SampleParcelable2/app/src/main/java/org/androidtown/sampleparcelable/MainActivity.java
@@ -0,0 +1,24 @@
+package org.androidtown.sampleparcelable;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+public static final int REQUEST_CODE_MENU = 101;
+    public static final String KEY_SIMPLE_DATA = "data";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Intent intent = new Intent(getApplicationContext(), MenuActivity.class);
+        SimpleData data = new SimpleData(100, "Hello android!");
+        intent.putExtra(KEY_SIMPLE_DATA, data);
+        startActivityForResult(intent, REQUEST_CODE_MENU);
+
+    }
+}
