db058950ddf925f92a424ddf4d574feb33249757
chlgpdus921 <chlgpdus921@naver.com>
2018-05-18 22:41:38 +0900

    practice

--- SampleProgress/app/src/main/java/org/androidtown/sampleprogress/MainActivity.java
+++ SampleProgress/app/src/main/java/org/androidtown/sampleprogress/MainActivity.java
@@ -5,13 +5,19 @@ import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
     ProgressDialog dialog;
-
+    private int brightness = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,5 +49,49 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        final TextView seekBarText = (TextView)findViewById(R.id.seekBarText);
+        Button button3 = (Button)findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                LinearLayout seekBarPanel = (LinearLayout)findViewById(R.id.seekBarPanel);
+                seekBarPanel.setVisibility(View.VISIBLE);
+            }
+        });
+
+        SeekBar seekBar = (SeekBar)findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                setBrightness(progress);
+                seekBarText.setText("시크바의 값 : "+progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+    private void setBrightness(int value){
+            if(value <10)
+                value = 10;
+            else if(value >100){
+                value= 100;
+            }
+
+            brightness = value;
+
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.screenBrightness = (float)value/100;
+        getWindow().setAttributes(params);
+    }
+
 }
