39143bef02cf4d2d79d16c505546866dca431de4
chlgpdus921 <chlgpdus921@naver.com>
2018-05-21 18:47:12 +0900

    practice

--- SamplePageSliding/app/src/main/java/org/androidtown/samplepagesliding/MainActivity.java
+++ SamplePageSliding/app/src/main/java/org/androidtown/samplepagesliding/MainActivity.java
@@ -1,7 +1,7 @@
-package org.androidtown.samplepagesliding;
+package org.techtown.samplepagesliding;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
@@ -9,11 +9,26 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    /**
+     * �럹�씠吏�媛� �뿴�젮 �엳�뒗吏� �븣湲� �쐞�븳 �뵆�옒洹�
+     */
     boolean isPageOpen = false;
 
+    /**
+     * �븷�땲硫붿씠�뀡 媛앹껜
+     */
     Animation translateLeftAnim;
     Animation translateRightAnim;
+
+    /**
+     * �뒳�씪�씠�뵫�쑝濡� 蹂댁뿬吏��뒗 �럹�씠吏� �젅�씠�븘�썐
+     */
     LinearLayout page;
+
+    /**
+     * 踰꾪듉
+     */
     Button button;
 
     @Override
@@ -21,11 +36,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button = (Button) findViewById(R.id.button);
+
+        // �뒳�씪�씠�뵫�쑝濡� 蹂댁뿬吏� �젅�씠�븘�썐 媛앹껜 李몄“
         page = (LinearLayout) findViewById(R.id.page);
 
+        // �븷�땲硫붿씠�뀡 媛앹껜 濡쒕뵫
         translateLeftAnim = AnimationUtils.loadAnimation(this, R.anim.translate_left);
         translateRightAnim = AnimationUtils.loadAnimation(this, R.anim.translate_right);
 
+        // �븷�땲硫붿씠�뀡 媛앹껜�뿉 由ъ뒪�꼫 �꽕�젙
         SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
         translateLeftAnim.setAnimationListener(animListener);
         translateRightAnim.setAnimationListener(animListener);
@@ -33,6 +53,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onButton1Clicked(View v) {
+        // �븷�땲硫붿씠�뀡 �쟻�슜
         if (isPageOpen) {
             page.startAnimation(translateRightAnim);
         } else {
@@ -41,7 +62,13 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class SlidingPageAnimationListener implements Animation.AnimationListener {
+    /**
+     * �븷�땲硫붿씠�뀡 由ъ뒪�꼫 �젙�쓽
+     */
+    private class SlidingPageAnimationListener implements Animation.AnimationListener {
+        /**
+         * �븷�땲硫붿씠�뀡�씠 �걹�궇 �븣 �샇異쒕릺�뒗 硫붿냼�뱶
+         */
         public void onAnimationEnd(Animation animation) {
             if (isPageOpen) {
                 page.setVisibility(View.INVISIBLE);
@@ -53,11 +80,15 @@ public class MainActivity extends AppCompatActivity {
                 isPageOpen = true;
             }
         }
+
         public void onAnimationRepeat(Animation animation) {
 
         }
 
         public void onAnimationStart(Animation animation) {
+
         }
+
     }
+
 }
--- SamplePageSliding_2/app/src/main/java/org/androidtown/samplepagesliding_2/MainActivity.java
+++ SamplePageSliding_2/app/src/main/java/org/androidtown/samplepagesliding_2/MainActivity.java
@@ -2,12 +2,63 @@ package org.androidtown.samplepagesliding_2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+    boolean isPageOpen = false;
+
+    Animation translateLeftAnim;
+    Animation translateRightAnim;
+    LinearLayout page;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        page = (LinearLayout) findViewById(R.id.page);
+        button = (Button) findViewById(R.id.button);
+        translateLeftAnim = AnimationUtils.loadAnimation(this, R.anim.translate_left);
+        translateRightAnim = AnimationUtils.loadAnimation(this, R.anim.translate_right);
+
+        SlidingPageAnimationListener animListener = new SlidingPageAnimationListener();
+        translateLeftAnim.setAnimationListener(animListener);
+        translateRightAnim.setAnimationListener(animListener);
+
+    }
+
+    public void onButton1Clicked(View v) {
+        if (isPageOpen) {
+            page.startAnimation(translateRightAnim);
+        } else {
+            page.setVisibility(View.VISIBLE);
+            page.startAnimation(translateLeftAnim);
+        }
+    }
+
+    private class SlidingPageAnimationListener implements Animation.AnimationListener {
+        public void onAnimationEnd(Animation animation) {
+            if (isPageOpen) {
+                page.setVisibility(View.INVISIBLE);
+
+                button.setText("Open");
+                isPageOpen = false;
+            } else {
+                button.setText("Close");
+                isPageOpen = true;
+            }
+        }
+
+        public void onAnimationRepeat(Animation animation) {
+
+        }
+
+        public void onAnimationStart(Animation animation) {
+        }
     }
 }
