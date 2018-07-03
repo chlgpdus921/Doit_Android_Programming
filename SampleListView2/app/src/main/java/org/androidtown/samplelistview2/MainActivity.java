22e7d5fd9768e78bf0c7d720a09399cd36297f26
chlgpdus921 <chlgpdus921@naver.com>
2018-07-03 22:14:55 +0900

    practice

--- SampleListView2/app/src/main/java/org/androidtown/samplelistview2/MainActivity.java
+++ SampleListView2/app/src/main/java/org/androidtown/samplelistview2/MainActivity.java
@@ -1,34 +1,28 @@
 package org.androidtown.samplelistview2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.ListView;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
-public class MainActivity extends AppCompatActivity {
-    TextView textView;
+public class MainActivity extends ListActivity {
     String[] items = {"mike", "angel", "crow", "john", "ginnie", "sally", "cohen", "rice"};
-ArrayAdapter arrayAdapter;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textView = (TextView) findViewById(R.id.textView);
-        setListAdapter(new ArrayAdapter<String >(this, android.R.layout.simple_list_item_1, items));
+        TextView textView = (TextView) findViewById(R.id.textView);
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
 
-protected void onListItemClick(ListView l, View v, int position, long id){
-            super.onListItemClick(l, v, position, id);
-            String text = "position: " + position + " " + items[position];
+    }
+    protected void onListItemClick(ListView I, View v, int position, long id){
+        super.onListItemClick(I, v, position, id);
+      //  textView.setText(items[position]);
+        String text = "position: " + position + " " + items[position];
 
-        }
-
-        adapter.setDropDownViewResource(android.R.layout.simple_list_item_1);
-
-         }
+    }
 }
