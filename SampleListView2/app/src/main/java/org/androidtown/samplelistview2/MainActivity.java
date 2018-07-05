b3d18bfba32493f68283f609121cefa087adc239
chlgpdus921 <chlgpdus921@naver.com>
2018-07-05 17:43:10 +0900

    practice

--- SampleListView2/app/src/main/java/org/androidtown/samplelistview2/MainActivity.java
+++ SampleListView2/app/src/main/java/org/androidtown/samplelistview2/MainActivity.java
@@ -5,27 +5,20 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-
+import android.widget.Toast;
 public class MainActivity extends ListActivity {
     String[] items = {"mike", "angel", "crow", "john", "ginnie", "sally", "cohen", "rice"};
-    TextView textView;
-    ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        listView = (ListView) findViewById(R.id.listView);
-        TextView textView = (TextView) findViewById(R.id.textView);
-      setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
-
+        setListAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items));
     }
 
-    protected void onListItemClick(ListView I, View v, int position, long id) {
-        super.onListItemClick(I, v, position, id);
-        textView.setText(items[position]);
-        String text = "position: " + position + " " + items[position];
 
+    protected void onListItemClick(ListView list, View v, int position, long id) {
+        super.onListItemClick(list, v, position, id);
+        String text = "position: " + position + " " + items[position];
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
     }
 }
