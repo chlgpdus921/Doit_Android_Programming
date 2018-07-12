7740a39231d338c8de7e55163d3105dcbb5af9b1
chlgpdus921 <chlgpdus921@naver.com>
2018-07-12 22:35:02 +0900

    practice"

---
+++ SampleCustomViewDrawable/app/src/main/java/org/androidtown/samplecustomviewdrawable/CustomViewDrawables.java
@@ -0,0 +1,12 @@
+package org.androidtown.samplecustomviewdrawable;
+
+import android.content.Context;
+import android.view.View;
+
+public class CustomViewDrawables extends View {
+    public CustomViewDrawables(Context context){
+        super(context);
+
+
+    }
+}
