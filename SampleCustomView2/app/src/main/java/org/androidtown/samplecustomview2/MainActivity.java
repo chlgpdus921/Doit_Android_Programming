36ebd576600437f226fba8a6fca4fe964628676c
chlgpdus921 <chlgpdus921@naver.com>
2018-07-12 20:38:15 +0900

    practice

--- SampleCustomView2/app/src/main/java/org/androidtown/samplecustomview2/MainActivity.java
+++ SampleCustomView2/app/src/main/java/org/androidtown/samplecustomview2/MainActivity.java
@@ -20,16 +20,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     protected class MyView extends View {
-        public MyView(Context context){
+        public MyView(Context context) {
             super(context);
         }
+
         /*onDraw 뷰가 화면에 보여질때 자동적으로 호출*/
         @Override
-        protected void onDraw(Canvas canvas){
+        protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             /*paint - 그래픽 그리기위한 속성으를 담고있다. */
             Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);//반듯하게 나옴.
-            paint.setStrokeWidth(8);
+           /* paint.setStrokeWidth(8);
             paint.setColor(Color.RED);
 
             paint.setStyle(Paint.Style.FILL);
@@ -40,10 +41,10 @@ public class MainActivity extends AppCompatActivity {
 
             paint.setStyle(Paint.Style.FILL_AND_STROKE);
             canvas.drawCircle(250,50,40,paint);
-         //    canvas.drawCircle(200,200,50,paint);
+         //    canvas.drawCircle(200,200,50,paint);*/
             /*중심측 좌표x, 중심측 좌표y, 반지름*/
 
-            paint.setColor(Color.BLUE);
+      /*      paint.setColor(Color.BLUE);
             paint.setStyle(Paint.Style.FILL);
             canvas.drawCircle(50,150,40,paint);
             paint.setColor(Color.RED);
@@ -56,7 +57,26 @@ public class MainActivity extends AppCompatActivity {
             canvas.drawCircle(150,150,40,paint);
             paint.setColor(Color.BLUE);
             paint.setStyle(Paint.Style.FILL);
-            canvas.drawCircle(150,150,40,paint);
+            canvas.drawCircle(150,150,40,paint);*/
+
+      /*CAP - 끝나는 지점의 장식을 설정 */
+            paint.setColor(Color.BLUE);
+            paint.setStrokeWidth(16);
+            canvas.drawLine(50, 30, 240, 30, paint);
+            paint.setStrokeCap(Paint.Cap.ROUND);
+            canvas.drawLine(50, 60, 240, 60, paint);
+            paint.setStrokeCap(Paint.Cap.SQUARE);
+            canvas.drawLine(50, 90, 240, 90, paint);
+
+            paint.setColor(Color.BLACK);
+            paint.setStrokeWidth(20);
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setStrokeJoin(Paint.Join.MITER);
+            canvas.drawRect(50,150,90,200,paint);
+            paint.setStrokeJoin(Paint.Join.BEVEL);
+            canvas.drawRect(120,150,160,200,paint);
+            paint.setStrokeJoin(Paint.Join.ROUND);
+            canvas.drawRect(190,150,230,200,paint);
         }
     }
 }
