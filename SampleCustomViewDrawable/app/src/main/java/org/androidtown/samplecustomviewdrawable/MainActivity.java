0a5e09321820485a60348bb9921468a87b895cb0
chlgpdus921 <chlgpdus921@naver.com>
2018-07-13 18:11:12 +0900

    practice

--- SampleCustomViewDrawable/app/src/main/java/org/androidtown/samplecustomviewdrawable/MainActivity.java
+++ SampleCustomViewDrawable/app/src/main/java/org/androidtown/samplecustomviewdrawable/MainActivity.java
@@ -1,13 +1,30 @@
 package org.androidtown.samplecustomviewdrawable;
 
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Gallery;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+LinearLayout mLinearLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLinearLayout = new LinearLayout(this);
+
+        ImageView img = new ImageView(this);
+        Drawable myImage = getResources().getDrawable(R.drawable.ic_launcher_background);
+        img.setImageDrawable(myImage);
+
+        img.setLayoutParams(new Gallery.LayoutParams(Gallery.LayoutParams.WRAP_CONTENT,
+                Gallery.LayoutParams.WRAP_CONTENT));
+
+        mLinearLayout.addView(img);
+
+        setContentView(mLinearLayout);
     }
 }
